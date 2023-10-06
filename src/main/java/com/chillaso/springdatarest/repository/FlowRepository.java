9821b858e2e3f0eaf6d4fb94c3e6c49b755ac527
Chillaso <carlos.chillagonzalez@gmail.com>
2023-10-06 13:15:44 +0200

    Add filter by name

--- src/main/java/com/chillaso/springdatarest/repository/FlowRepository.java
+++ src/main/java/com/chillaso/springdatarest/repository/FlowRepository.java
@@ -2,8 +2,10 @@ package com.chillaso.springdatarest.repository;
 
 import com.chillaso.springdatarest.entity.Flow;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel= "flows", path = "flows")
 public interface FlowRepository extends JpaRepository<Flow, Long> {
+    Flow findByName(@Param("name") String name);
 }
